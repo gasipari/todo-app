9243d32770b8316609b3f7194c802fc37f0ab94c
gasipari <riamarius@gmail.com>
2016-05-24 21:02:23 -0400

    Add filter support

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -50,10 +50,52 @@ describe('TodoAPI', () => {
 
       localStorage.setItem('todos', JSON.stringify(todos));
       let actualTodos = TodoAPI.getTodos();
-      
+
       expect(actualTodos).toEqual(todos);
     });
-
   });
+
+  describe('filterTodos', () => {
+    let todos = [{
+      id: 1,
+      text: 'Test item 1',
+      completed: true
+    },
+    {
+      id: 2,
+      text: 'Test item 2',
+      completed: false
+    },
+    {
+      id: 3,
+      text: 'Some Test item 3',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return uncompleted items if showCompleted is false', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    })
+
+    it('should filter todos by searchText', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      let filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+  })
 
 })
